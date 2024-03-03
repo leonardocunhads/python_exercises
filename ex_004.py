c2c037dd1af1434dbbfe4538fbfc8fb387b898bb
Leonardo Cunha <leonardocunhads@gmail.com>
2024-03-03 09:47:13 +0000

    Adding the .format() on some prints function

--- ex_004.py
+++ ex_004.py
@@ -2,16 +2,16 @@ coisa = input('Insert something: ')
 # type() -> checks the variable's type 
 print('The tpyte is: ', type(coisa))
 # isspace() -> checks if what was entered contains only spaces
-print('Are there only spaces?', coisa.isspace())
+print('Are there only spaces? {}'.format(coisa.isspace()))
 # isnumeric() -> checks if what was entered is a number
 print('Is it a number? ', coisa.isnumeric())
 # isalpha() -> checks if what was entered is alphabetical
-print('Is it alphabetical? ', coisa.isalpha())
+print('Is it alphabetical? {}'.format(coisa.isalpha()))
 # isalnum() -> checks if what was entered is alphanumeric
 print('Is it alphanumerical? ', coisa.isalnum())
-# isalnum() -> checks if what was entered is upper
-print('Is it upper? ', coisa.isupper())
-# isalnum() -> checks if what was entered is upper
+# isupper() -> checks if what was entered is upper
+print('Is it upper? {}'.format(coisa.isupper()))
+# islower() -> checks if what was entered is upper
 print('Is it lower? ', coisa.islower())
 # istitle() -> checks if what was entered is capitalized
-print('Is it Capitalized? ', coisa.istitle())
+print('Is it Capitalized? {}'.format(coisa.istitle()))
