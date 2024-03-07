69c83af603716b5bbe4a50b0b12bc78885feae4c
Leonardo Cunha <leonardocunhads@gmail.com>
2024-03-07 13:11:48 +0000

    first commit of the day

---
+++ ex_014.py
@@ -0,0 +1,23 @@
+price = 10
+
+discount = 0.5
+
+result = price * (1 - discount)
+
+print('The Result is R${:.2f}'.format(result))
+
+name = 'Ruã'
+
+name = 'Bruno'
+
+print(name)
+
+print(name * 2)
+
+print(name + name)
+
+a = 25
+
+b = a
+print(a)
+print(b)
---
+++ ex_015.py
@@ -0,0 +1,15 @@
+# F-STRING
+
+name = 'José'
+
+greeting = 'Hello, Bruno'
+
+print(greeting)
+
+# -------------------------------
+
+greeting = f'Hello, {name}'
+print(greeting)
+
+greeting = 'Hello, {}'.format(name)
+print(greeting)
